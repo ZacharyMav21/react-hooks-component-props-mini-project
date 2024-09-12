479209427d230fb560af5a8d85ac392f69dd6d48
Zachary <zepm0122.zn@gmail.com>
2024-09-12 16:26:57 -0400

    alt added and default date

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,16 @@
 function Article({ blog }) {
-    let numCups = Math.ceil((blog.minutes) / 5)
 
-	return (
-		<article>
-			<h3>{blog.title}</h3>
-			<small>{blog.date} {'☕️'.repeat(numCups)} {blog.minutes} min read</small>
-			<p>{blog.preview}</p>
-		</article>
-	);
+    let numCups = Math.ceil(blog.minutes / 5);
+    
+    const date = blog.date || "January 1, 1970";
+
+    return (
+        <article>
+            <h3>{blog.title}</h3>
+            <small>{date} {'☕️'.repeat(numCups)} {blog.minutes} min read</small>
+            <p>{blog.preview}</p>
+        </article>
+    );
 }
 
 export default Article;
